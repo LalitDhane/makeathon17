22953cf3fb00d5684618b12f38eb23704f31d1b9
LalitDhane <lalitdhane4455@gmail.com>
2024-06-08 18:36:56 +0530

    home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,25 @@ import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { SharedModule } from "./shared/shared.module";
+import { HomeComponent } from "./home/home.component";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatInputModule } from "@angular/material/input";
+import { MatIconModule } from "@angular/material/icon";
+import { MatCardModule } from "@angular/material/card";
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
     SharedModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule,
+    MatCardModule,
+    FlexLayoutModule,
+    MatButtonModule,
     AppRoutingModule,
     BrowserAnimationsModule,
   ],
